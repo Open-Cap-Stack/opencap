228865219cf5b71ec1bca9c7eda140ab4d0a6970
jellyfish <rvupmo33@gmail.com>
2024-07-22 13:11:54 -0400

    WIP: Activity Routes

--- models/Activity.js
+++ models/Activity.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ActivitySchema = new Schema({
+  activityId: { type: String, unique: true, required: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, required: true },
   type: { type: String, required: true },
-  participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
   status: { type: String, required: true },
-  createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdBy: { type: Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Activity', ActivitySchema);
+module.exports = mongoose.model("Activity", ActivitySchema);
