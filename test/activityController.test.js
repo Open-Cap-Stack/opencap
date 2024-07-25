93cad914bebc529a584dddc759be12aaafa96e26
YazmynS <yzsims@ucsc.edu>
2024-07-25 09:59:45 -0700

    WIP Admin

---
+++ test/activityController.test.js
@@ -0,0 +1,7 @@
+const chai = require("chai");
+const sinon = require("sinon");
+const mongoose = require("mongoose");
+const Activity = require("../models/Activity");
+const activityController = require("../controllers/activityController");
+
+const expect = chai.expect;
