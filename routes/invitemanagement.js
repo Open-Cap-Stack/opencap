ca5e8cb53291d6a877f0b5e2f0b01720b66c65b9
jellyfish <rvupmo33@gmail.com>
2024-07-30 12:23:56 -0400

    WIP: Invite Management

---
+++ routes/invitemanagement.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const inviteManagementController = require("../controllers/inviteManagementController");
+
+// Route for creating an invite
+router.post("/invites", inviteManagementController.createInvite);
+
+// Route for getting all invites
+router.get("/invites", inviteManagementController.getAllInvites);
+
+// Route for getting an invite by ID
+router.get("/invites/:id", inviteManagementController.getInviteById);
+
+// Route for updating an invite
+router.put("/invites/:id", inviteManagementController.updateInvite);
+
+// Route for deleting an invite
+router.delete("/invites/:id", inviteManagementController.deleteInvite);
+
+module.exports = router;
