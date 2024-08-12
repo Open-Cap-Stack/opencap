ca502b00db730fb0a46edffe25667ef6401f90ff
anushac14 <113647676+anushac14@users.noreply.github.com>
2024-08-12 15:20:49 -0400

    Tax Calculator Green Test

--- routes/TaxCalculatorRoutes.js
+++ routes/TaxCalculatorRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const TaxCalculator = require('../models/TaxCalculator');
+const TaxCalculator = require('../models/TaxCalculatorModel');
 
 const router = express.Router();
 
