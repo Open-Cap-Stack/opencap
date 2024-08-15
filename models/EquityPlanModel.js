8407bae950d88aa3a4d30a235ee5fa2f87fefc8d
anushac14 <113647676+anushac14@users.noreply.github.com>
2024-08-15 03:07:56 -0400

    Equity Plan Green Tests

--- models/EquityPlanModel.js
+++ models/EquityPlanModel.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 
 const equityPlanSchema = new mongoose.Schema({
+    planId: { type: String, unique: true, required: true },
     planName: { type: String, required: true },
     description: { type: String },
     startDate: { type: Date, required: true },
     endDate: { type: Date },
-    allocation: { type: Number, required: true },  // Amount of equity allocated
-    participants: [{ type: String }]  // List of participant names or IDs
+    allocation: { type: Number, required: true },
+    participants: [{ type: String }], // List of stakeholder names or IDs
+    VestingTerms: { type: Object },
+    VestingStartDate: { type: Date },
+    VestingEndDate: { type: Date },
+    VestingSchedule: { type: String },
+    PlanType: { type: String, enum: ['Stock Option Plan', 'Restricted Stock Plan'], required: true },
+    AllocationType: { type: String, enum: ['Fixed', 'Performance-Based'] },
+    PlanAdministrator: { type: String }
 }, { timestamps: true });
 
 const EquityPlan = mongoose.model('EquityPlan', equityPlanSchema);
