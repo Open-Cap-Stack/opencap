801c7590e2e6f225503c0b1dd41c4e3108ea9c90
QTravel Developer <demo@qtravel.example>
2025-05-18 00:15:28 -0700

    WIP: OCDI-303: Fix document routes to correct API endpoint path structure

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const documentController = require('../controllers/documentController');
 
-router.post('/documents', documentController.createDocument);
-router.get('/documents', documentController.getDocuments);
-router.get('/documents/:id', documentController.getDocumentById);
-router.put('/documents/:id', documentController.updateDocumentById);
-router.delete('/documents/:id', documentController.deleteDocumentById);
+router.post('/', documentController.createDocument);
+router.get('/', documentController.getDocuments);
+router.get('/:id', documentController.getDocumentById);
+router.put('/:id', documentController.updateDocumentById);
+router.delete('/:id', documentController.deleteDocumentById);
 
 module.exports = router;
